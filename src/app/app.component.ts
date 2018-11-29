f9901a5e295cc269880d90ed5fe6f5ab50dea626
Mitchell Van Cleave <darealmvc@gmail.com>
2018-11-29 15:58:18 -0500

    need to fix the fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
+import { FilmsService} from './films.service';
+import { PeopleService } from './people.service';
+import { ShipsService } from './ships.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'swapi';
+  myForm: FormGroup;
+  info: Object;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private filmsService: FilmsService,
+    private peopleService: PeopleService,
+    private shipsService: ShipsService) { }
+
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      menu: new FormControl(),
+      getData: new FormControl(),
+    })
+  }
+  submit() {
+    if (this.myForm.value.menu === 'films') {
+      this.filmsService.getFilms(this.myForm.value.getData)
+        .subscribe(data => {
+          this.info = data
+        })
+    }else if (this.myForm.value.menu === 'people') {
+      this.peopleService.getPeople(this.myForm.value.getData)
+        .subscribe(data => {
+          this.info = data
+        })
+    }else if (this.myForm.value.menu === "ships"){
+      this.shipsService.getShips(this.myForm.value.getData)
+        .subscribe(data =>{
+          this.info = data
+        })
+    } else{
+      console.log('Error')
+    }
+    
+  }
 }
---
+++ src/app/ships.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShipsService {
+
+  constructor(private http: HttpClient) { }
+
+  getShips(getData) {
+    return this.http.get(`https://swapi.co/api/starships/?search=${getData}`)
+  }
+}
